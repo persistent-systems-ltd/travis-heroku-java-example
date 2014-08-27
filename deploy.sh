85d1254dbc6358330cd06221ab965d33a430c6fe
Joe Kutner <jpkutner@gmail.com>
2014-08-27 14:41:11 -0500

    added some logging

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ APP_NAME="rocky-ocean-6373"
 APP_FILE="${APP_NAME}.tar.gz"
 
 echo "---> Creating the release package..."
-tar cf ${APP_FILE} target/dependency target/*.war Procfile
+tar cvf ${APP_FILE} target/dependency target/*.war Procfile
 
 # create the Auth token
 HK_TOKEN=`(echo -n ":" ; echo "${HEROKU_API_TOKEN}") | base64`
@@ -11,6 +11,11 @@ HK_TOKEN=`(echo -n ":" ; echo "${HEROKU_API_TOKEN}") | base64`
 HK_SOURCES=$(curl -s -X POST https://api.heroku.com/apps/${APP_NAME}/sources -H 'Accept: application/vnd.heroku+json; version=3' -H "Authorization: ${HK_TOKEN}")
 HK_GET_URL=$(expr "$HK_SOURCES" : ".*\"get_url\":\"\(https://.*\)\",")
 HK_PUT_URL=$(expr "$HK_SOURCES" : ".*\"put_url\":\"\(https://.*\)\"")
+
+echo "TOK: $HK_TOKEN"
+echo "SRC: $HK_SOURCES"
+echo "GET: $HK_GET_URL"
+echo "PUT: $HK_GET_URL"
 
 echo "---> Uploading release package..."
 curl "$HK_PUT_URL" -X PUT -H 'Content-Type:' --data-binary @${APP_FILE}
