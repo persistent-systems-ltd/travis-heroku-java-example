dfb3986bc9fe5e41fe025f748ef7d1f396a28df4
Joe Kutner <jpkutner@gmail.com>
2014-08-27 15:04:02 -0500

    trying to get the auth token right

--- deploy.sh
+++ deploy.sh
@@ -12,10 +12,10 @@ HK_SOURCES=$(curl -s -X POST https://api.heroku.com/apps/${APP_NAME}/sources -H 
 HK_GET_URL=$(expr "$HK_SOURCES" : ".*\"get_url\":\"\(https://.*\)\",")
 HK_PUT_URL=$(expr "$HK_SOURCES" : ".*\"put_url\":\"\(https://.*\)\"")
 
-echo "TOK: $HK_TOKEN"
-echo "SRC: $HK_SOURCES"
-echo "GET: $HK_GET_URL"
-echo "PUT: $HK_GET_URL"
+# echo "TOK: $HK_TOKEN"
+# echo "SRC: $HK_SOURCES"
+# echo "GET: $HK_GET_URL"
+# echo "PUT: $HK_GET_URL"
 
 echo "---> Uploading release package..."
 curl "$HK_PUT_URL" -X PUT -H 'Content-Type:' --data-binary @${APP_FILE}
