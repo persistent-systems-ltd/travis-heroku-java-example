d328de15f41c866c689ea3a1898d9b65f02ee124
Joe Kutner <jpkutner@gmail.com>
2014-08-27 14:50:53 -0500

    trying to get the auth token right

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,17 @@ echo "---> Creating the release package..."
 tar cvf ${APP_FILE} target/dependency target/*.war Procfile
 
 # create the Auth token
-HK_TOKEN=`(echo -n ":" ; echo "${HEROKU_API_KEY}") | base64`
+HK_TOKEN=`(echo -n ":" ; echo "${1:-HEROKU_API_KEY}") | base64`
 
 # retrieve the URLs for putting and getting the archive
 HK_SOURCES=$(curl -s -X POST https://api.heroku.com/apps/${APP_NAME}/sources -H 'Accept: application/vnd.heroku+json; version=3' -H "Authorization: ${HK_TOKEN}")
 HK_GET_URL=$(expr "$HK_SOURCES" : ".*\"get_url\":\"\(https://.*\)\",")
 HK_PUT_URL=$(expr "$HK_SOURCES" : ".*\"put_url\":\"\(https://.*\)\"")
+
+# echo "TOK: $HK_TOKEN"
+# echo "SRC: $HK_SOURCES"
+# echo "GET: $HK_GET_URL"
+# echo "PUT: $HK_GET_URL"
 
 echo "---> Uploading release package..."
 curl "$HK_PUT_URL" -X PUT -H 'Content-Type:' --data-binary @${APP_FILE}
